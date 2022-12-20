5e5cdfe9b80efdd728845d212285ab89d864c322
QuocHuy0808 <huy.quoc.nguyen@evizi.com>
2022-12-20 15:15:37 +0700

    update my item

--- src/util/getMediaType.tsx
+++ src/util/getMediaType.tsx
@@ -1,3 +1,7 @@
 export const getMediaType = (src: string): 'image' | 'video' => {
-  return ['jpg', 'gif', 'png', 'webp'].includes(new URL(src).pathname.split('.')[1]) ? 'image' : 'video';
+  return ["jpg", "gif", "png", "webp", "jpeg"].includes(
+    new URL(src).pathname.split(".")[1]
+  )
+    ? "image"
+    : "video";
 };
