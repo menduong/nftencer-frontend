ebe0fe6d62cd3e7f8211f80d5571909c9248cea9
QuocHuy0808 <huy.quoc.nguyen@evizi.com>
2023-01-06 16:53:39 +0700

    update sell 1155

---
+++ src/store/sellNFT/constants/index.ts
@@ -0,0 +1,8 @@
+import { CreateForm, CreateFormData } from "components/pages/create/form";
+import { AnyArray } from "immer/dist/internal";
+
+export type Error = any;
+
+export type ReSellNFTReq = any;
+
+export type ReSellNFTRes = any;
